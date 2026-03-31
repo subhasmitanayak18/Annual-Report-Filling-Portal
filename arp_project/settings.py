f2e20f16c3d77772f6c5daae8c8d07808dc3e836
subhasmitanayak18 <subhasmitanayak2005@gmail.com>
2026-03-31 11:52:40 +0530

    Fixed static files using whitenoise

--- arp_project/settings.py
+++ arp_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -107,3 +108,4 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8080',
     'http://localhost:8080',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
